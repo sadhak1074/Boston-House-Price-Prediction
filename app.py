330e709a544fe2521acbd6d44df821c2fe61e262
Sadhak Saini <sadhaksaini18@gmail.com>
2023-03-25 10:53:25 +0530

    s

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ regmodel=pickle.load(open('regmodel.pkl','rb'))
 scalar=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
@@ -29,7 +29,7 @@ def predict():
     final_input=scalar.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output=regmodel.predict(final_input)[0]
-    return render_template("index.html",prediction_text="The House price prediction is {}".format(output))
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
 
 
 
